278e94d0acd0e722648b599de1245a579c0272f7
Cr0mb <137664526+Cr0mb@users.noreply.github.com>
2024-12-15 17:06:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [109600, 730, 10, 30, 40, 1085660, 130, 70, 50, 20, 60, 440];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
