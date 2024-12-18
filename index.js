b87efa0a334a12c554d5cb6d58b20ebeafbabcb7
Cr0mb <137664526+Cr0mb@users.noreply.github.com>
2024-12-18 03:48:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const steamUser = require('steam-user');
 const readline = require('readline');
-const keep_alive = require('./keep_alive.js');
+const http = require('http');
 
+// Environment variables for Steam account credentials
 var username = process.env.username;
 var password = process.env.password;
 
 var games = [10, 20, 30, 40, 50, 60, 70, 130, 440, 730, 22330, 22380, 33230, 238960, 48190, 109600, 1085660, 1857950];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
+// Create Steam user instance
 user = new steamUser();
 
 const rl = readline.createInterface({
@@ -15,6 +17,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// Steam Guard code prompt
 user.on('steamGuard', (domain, callback) => {
     console.log('Steam Guard code sent to your email');
     rl.question('Enter the Steam Guard code sent to your email: ', (code) => {
@@ -23,6 +26,7 @@ user.on('steamGuard', (domain, callback) => {
     });
 });
 
+// Event listener for successful login
 user.on('loggedOn', () => {
     if (user.steamID != null) {
         console.log(user.steamID + ' - Successfully logged on');
@@ -31,16 +35,16 @@ user.on('loggedOn', () => {
     user.gamesPlayed(games);
 });
 
+// Log on to Steam
 user.logOn({
     accountName: username,
     password: password
 });
 
-keep_alive.js
-
-var http = require('http');
-
+// Create a basic HTTP server to keep the bot alive
 http.createServer(function (req, res) {
   res.write("I'm alive");
   res.end();
-}).listen(8080);
+}).listen(80, () => {
+  console.log("Keep alive server running on port 80");
+});
