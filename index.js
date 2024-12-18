4375d3ac439c85d2946fa8a92f68378db127b540
Cr0mb <137664526+Cr0mb@users.noreply.github.com>
2024-12-18 03:33:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var username = process.env.username;
 var password = process.env.password;
 
 var games = [
-	10, 20, 30, 40, 50, 60, 70, 130, 440, 730, 22330, 22380, 33230, 238960, 48190,
-	109600, 1085660, 1857950,
+    10, 20, 30, 40, 50, 60, 70, 130, 22380, 39500, 39510, 489830, 109600, 2100, 65540, 704450, 950670
 ]; // Enter here AppIDs of the needed games
 var status = 1; // 1 - online, 7 - invisible
 
@@ -14,17 +13,10 @@ user = new steamUser();
 
 user.on("steamGuard", (domain, callback) => {
 	console.log("Steam Guard code sent to your email");
-	// Here, you can implement a manual input prompt for the user to input the email code.
-	// For simplicity, this example will just log the message and wait for the user input
-	// You can replace this part with a more automated system if desired.
-
-	// Prompt the user to enter the Steam Guard code received in their Gmail.
-	// This can be a manual process or fully automated (though automating Gmail access is more complex).
 
 	const prompt = require("prompt-sync")();
 	const code = prompt("Enter the Steam Guard code sent to your email: ");
 
-	// Call the callback to submit the code received via email
 	callback(code);
 });
 
